54da18bc23360a5a2aa6e434b877b5aba4275bf2
JennTheWeb <jenncarr530@GMAIL.COM>
2017-12-08 15:19:07 -0500

    integrate ball movement with brick.html

---
+++ ball.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    var bounceArea = document.getElementById("bounceArea");
+    var ball = document.getElementById("ball");
+
+    var x = 60;
+    var y = 0;
+
+    var dx = 1;
+    var dy = -2;
+
+    function draw() {
+
+        x += dx;
+        y += dy;
+
+        ball.style.left = ""+x+"px";
+        ball.style.top = ""+y+"px";
+
+        if(x + dx > bounceArea.scrollWidth || x + dx < 0) {
+            dx = -dx;
+        }
+        if(y + dy > bounceArea.scrollHeight || y + dy < 0) {
+            dy = -dy;
+        }
+    }
+    setInterval(draw, 10);
+
+});
