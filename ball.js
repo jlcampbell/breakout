f11b216d7b3c4e26971d07f1fa62dda0e99dadec
jlcampbell <jlcampbell@users.noreply.github.com>
2017-12-10 11:47:20 -0500

    ball starts in center of screen and moves downward first

--- ball.js
+++ ball.js
@@ -7,16 +7,15 @@ function dropBall() {
     var ballRadius = 10;
     var score = 0;
 
-    let x = 60;
-    let y = 0;
+    let x = bounceArea.scrollWidth/2;
+    let y = bounceArea.scrollHeight/2;
 
 
 
     drawBricks();
 
-
     var dx = 1;
-    var dy = -2;
+    var dy = 2;
 
     //setInterval(bounce, 10);
     bounce();
@@ -35,22 +34,22 @@ function dropBall() {
 
 
         function collisionDetection() {
-            console.log("detecting")
 
-            console.log(x);
-
-            console.log(y);
             for(c=0; c<brickColumnCount; c++) {
                 for(r=0; r<brickRowCount; r++) {
                     var b = bricks[c][r];
                     if(b.status == 1) {
+                      //position of ball is inside brick
                       if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                        //reverse y direction
                         dy = -dy;
+                        //record that the brick is hit
                         b.status = 0;
-                        console.log("collision");
-                        //on collision, make brick disappear
+                        //hide brick
                         document.getElementById(b.id).style.display = "none";
+                        //increase score
                         score++;
+                        //if all bricks are hit, game is over
                         if(score == brickRowCount*brickColumnCount) {
                         alert("YOU WIN, CONGRATULATIONS!");
                         document.location.reload();
@@ -63,7 +62,7 @@ function dropBall() {
 
         function drawScore() {
           document.getElementById("score").innerHTML = score;
-          //ctx.fillText("Score: "+score, 8, 20);
+
 }
         drawScore();
         collisionDetection();
@@ -82,8 +81,8 @@ function dropBall() {
             }
         }
         if( x + dx > bounceArea.scrollWidth-ballRadius || x + dx < 0 ) {
-    //        dx = Math.abs(dx) < 8 ? -dx * 1.2 : -dx;
-              dx = -dx;
+            dx = Math.abs(dx) < 8 ? -dx * 1.2 : -dx;
+    //          dx = -dx;
         }
         if( y + dy < 0 ) {
             dy = Math.abs(dy) < 16 ? dy = -dy * 1.2 : -dy;
