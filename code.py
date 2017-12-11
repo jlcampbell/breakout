f80b87c0b441c5e9b7c994f08202189f367c2a1f
jlcampbell <jlcampbell@users.noreply.github.com>
2017-12-11 10:00:57 -0500

    made paddle less shaky code.py

--- code.py
+++ code.py
@@ -13,23 +13,14 @@ diff = 0
 
 while True:
     x, y, z = cpx.acceleration
-    val = math.floor((x-x2)*10)
-    if 0.5 > x-x2 > 0.3:
-        x2 = x;
-        y2 = y;
-        z2 = z;
-        m.move(-10, 0, 0)
+
     if x-x2>0.5:
         print(x-x2)
         x2 = x;
         y2 = y;
         z2 = z;
         m.move(-20, 0, 0)
-    elif -0.5< x-x2 < -0.3:
-        x2 = x;
-        y2 = y;
-        z2 = z;
-        m.move(10, 0, 0)
+
     elif x-x2<-0.5:
         print(x-x2)
 
